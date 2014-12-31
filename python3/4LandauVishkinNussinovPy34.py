b2a3e83f7d71542d3c445aba2eea65482dc34881
Tincektigar <Tincektigar@users.noreply.github.com>
2014-12-31 16:33:44 +0100

    L i T dodani, while uvjet popravljen, ##novi simboli kasnije

--- python3/4LandauVishkinNussinovPy34.py
+++ python3/4LandauVishkinNussinovPy34.py
@@ -22,22 +22,42 @@ for i in range(m):
         MAXLENGTH[i][j] = length
         MAXLENGTH[j][i] = length
         
-print(MAXLENGTH)
-   
 
-j=0
+j = 0
+choice = 0
 for i in range(n-m+k+1):
-    L[d,d-2]=L[d,-d-2]=NEG_INF
-    if d<0:
-        L[d,abs(d)-1]=abs(d)-1
+    L = dict()
+    T = dict()
+    
+    L[(d, abs(d)-2)] = -999
+    if (d < 0):
+        L[d, abs(d)-1] = abs(d)-1
     else:
-        L[d,abs(d)-1]=-1
+        L[d, abs(d)-1] = -1
+        
     for e in range(k+1):
         for d in range(-e, e+1):
-            row=max((L[d,e-1]+1),L[d-1,e-e],(L[d+1,e-1]+1))
+            row = max(L[(d, e-1)]+1, L[(d-1, e-1)], L[(d+1, e-1)]+1)
+            
+            print(row)
+            
+            if (row == L[(d-1, e-1)]):
+                    choice = 0
+                    T[(d, e)] = T[(d-1, e-1)]
+                    T[(d, e)].append((i+row+d-1,0,0))
+                elif (row == L[(d, e-1)]+1):
+                    choice = 1
+                    T[(d, e)] = T[(d, e-1)]
+                    T[(d, e)].append((i+row+d-1,0,0))
+                elif (row == L[(d+1, e-1)]+1):
+                    choice = 2
+                    T[(d, e)] = T[(d+1, e-1)]
+                
+            print(T(d, e)]
+                
             done=0
-            while (i+row+d+1)<=j and done==0 and row<m:
-                ##neki tripleti
+            while ((i+row+d+1) <= j) and (row < m) and ((i+row+d) < n):
+                ##c i f
                 if f>=1:
                     if f!=MAXLENGHT[c][row]:
                         row=row+min(f,MAXLENGHT(c,row))

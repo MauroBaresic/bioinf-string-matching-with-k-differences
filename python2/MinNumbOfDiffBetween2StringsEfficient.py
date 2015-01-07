a6d2b2ce2437121a5f09b0d67d337d2da576135c
MauroBaresic <mauro.baresic@outlook.com>
2015-01-07 23:04:15 +0100

    Update MinNumbOfDiffBetween2StringsEfficient.py

--- python2/MinNumbOfDiffBetween2StringsEfficient.py
+++ python2/MinNumbOfDiffBetween2StringsEfficient.py
@@ -2,23 +2,18 @@
 # email: mauro.baresic@outlook.com
 
 import sys
-import numpy as np
-#import psutil
 import os
 import time
-"""
-def memory_usage_psutil():
-    # return the memory usage in MB    
-    process = psutil.Process(os.getpid())
-    mem = process.get_memory_info()[0] / float(2 ** 20)
-    return str(round(mem,3))
-"""
+
+# procedure for parsing file in FASTA format
 def parseFastaFile(path):
     f = open(path,'r')
     sequence = ""
     readingSequence = False
     for row in f.readlines():
-        if (row[0] == '>' and readingSequence == False):
+        if (row[0] == ','):
+            continue
+        elif (row[0] == '>' and readingSequence == False):
             readingSequence = True
         elif (row[0] != '>' and readingSequence == True):
             sequence += row.rstrip()
@@ -27,18 +22,41 @@ def parseFastaFile(path):
     f.close()
     return sequence
 
+# class that implements Landau-Vishkin-Nussinov algorithm
 class MinDifferenceEfficient():
+    
+    # pattern string
     R = ''
+    
+    # text string
     B = ''
+    
+    # length of pattern
     m = 0
+    
+    # length of text
     n = 0
+    
+    # maximal number of allowed differences
     k = 0
+    
+    # hashtable L[d,e] that denotes the largest
+    # row i on diagonal d with e differences 
     L = None
+    
+    # [m x m] matrix for pattern analysis
     MAXLENGTH = None
+    
+    # hash for all sequences of triples (p,c,f)
     T = None
+    
+    # current sequence of triples (p,c,f)
     Sij = None
+    
+    # pair (d,e) that denotes new current sequence
     newSij = None
     
+    # constructor
     def __init__(self,R,B):
         self.R = R
         self.B = B
@@ -47,11 +65,15 @@ class MinDifferenceEfficient():
         self.L = dict()
         self.T = dict()
         self.patternAnalysis()
-
+    
+    # procedure that computes pattern analysis
     def patternAnalysis(self): # O(m * m *m)
         if ( len(self.R) < 1 ):
             return -1
-        self.MAXLENGTH = np.zeros((self.m,self.m),dtype = np.int)
+        self.MAXLENGTH = dict()
+        for i in xrange(self.m):
+            for j in xrange(self.m):
+                self.MAXLENGTH[(i,j)] = 0
         for i in xrange(self.m):
             for j in xrange(i,self.m):
                 length = 0
@@ -62,7 +84,8 @@ class MinDifferenceEfficient():
                 self.MAXLENGTH[(i,j)] = length
                 self.MAXLENGTH[(j,i)] = length
         return 0
-
+        
+    # procedure that finds covering of index h in sequence of triples (p,c,f)
     def findCovering(self, S, h, idx): #returns (int, (int, int, int))
         #S = [(20,3,1),(21,0,0),(22,5,2),(24,0,0),(25,7,3),(28,11,1),(29,0,0)]
         id_last = idx
@@ -75,13 +98,12 @@ class MinDifferenceEfficient():
                 id_last = c
                 break
         if (S[id_last][2] == 0):
-            #print idx, id_last
             return (id_last, S[id_last])
         else:
-            #print idx, id_last
             difference = h - S[id_last][0]
             return (id_last,(difference + S[id_last][0], difference + S[id_last][1], S[id_last][2] - difference))
 
+    # procedure for calculating Landau-Vishkin-Nussinov algorithm
     def calculate(self, k):
         self.k = k
         if (self.MAXLENGTH is None):
@@ -90,21 +112,22 @@ class MinDifferenceEfficient():
         j = 0
         max_j = 0
         idx = 0
+        
         for i in xrange(self.n - self.m + self.k + 1):
-
+            next_i = False
             self.L = dict()
             self.T = dict()
             
-            #1
+            #1 Initialization
             for d in xrange(-(self.k + 1), (self.k + 1) + 1):
                 self.L[(d,abs(d)-2)] = - float("inf")
-                self.T[(d,abs(d)-2)] = list()#
+                self.T[(d,abs(d)-2)] = list()
                 if (d < 0):
                     self.L[(d,abs(d)-1)] = abs(d) - 1
-                    self.T[(d,abs(d)-1)] = list()#
+                    self.T[(d,abs(d)-1)] = list()
                 else:
                     self.L[(d,abs(d)-1)] = -1
-                    self.T[(d,abs(d)-1)] = list()#
+                    self.T[(d,abs(d)-1)] = list()
 
             #2
             for e in xrange(self.k + 1):
@@ -116,30 +139,26 @@ class MinDifferenceEfficient():
                     
                     if (row == self.L[(d-1,e-1)]):
                         idx = 0
-                        self.T[(d,e)] = self.T[(d-1,e-1)][:]#
-                        self.T[(d,e)].append((i+row+d-1,0,0))#
+                        self.T[(d,e)] = self.T[(d-1,e-1)][:]
+                        self.T[(d,e)].append((i+row+d-1,0,0))
                     elif (row == self.L[(d,e-1)] + 1):
                         idx = 1
-                        self.T[(d,e)] = self.T[(d,e-1)][:]#
-                        self.T[(d,e)].append((i+row+d-1,0,0))#
+                        self.T[(d,e)] = self.T[(d,e-1)][:]
+                        self.T[(d,e)].append((i+row+d-1,0,0))
                     elif (row == self.L[(d+1,e-1)] + 1):
                         idx = 2
-                        self.T[(d,e)] = self.T[(d+1,e-1)][:]#
+                        self.T[(d,e)] = self.T[(d+1,e-1)][:]
                         
                     #4 new
                     GoTo = False
-                    #print i + row + d + 1, j, d, e
                     while ((row < self.m) and (i + row + d < self.n) and ( (i + row + d + 1) <= j)):
                         #4 new.1
                         c=0
                         f=0
                         if (self.Sij is not None):
-                            #print "i,j:",i,j
                             last_id, triple = self.findCovering(self.Sij,i+row+d,0)
                             c = triple[1]
                             f = triple[2]
-                        else:
-                            print i,d,e,j
                         #4 new.2
                         if (f >= 1): #case a
                             #4 new.3
@@ -152,16 +171,13 @@ class MinDifferenceEfficient():
                                 row += f
                         else: #case b
                             #4 new.4
-                            try:
-                                if (self.B[i + row + d] != self.R[row]): #case b1
-                                    #GOTO 5
-                                    GoTo = True
-                                    break
-                            except IndexError:
-                                print row, i+row+d+1, j
+                            if (self.B[i + row + d] != self.R[row]): #case b1
+                                #GOTO 5
+                                GoTo = True
+                                break
                             else: #case b2
                                 row += 1
-
+                            
                     if (False == GoTo):
                         #4 old
                         while ((row < self.m) and (i + row + d < self.n) and (self.R[row] == self.B[i + row + d])):
@@ -177,17 +193,25 @@ class MinDifferenceEfficient():
                         self.newSij = (d,e)
                     #6
                     if (row == self.m):
-                        print "An occurence with <=",self.k,"differences of the pattern starts at",i
-                        #print memory_usage_psutil()
-                        #GOTO 7 ili ne
+                        print "An occurence with <=",self.k,"differences of the pattern starts in text at",i,", ends at",max_j
+                        print self.R, self.B[i:max_j+1]
+                        #GOTO 7
+                        next_i = True
+                        break
+                
+                #GOTO 7
+                if (next_i == True):
+                    break
                         
             #7
             if (max_j > j):
                 j = max_j
                 self.Sij = self.T[self.newSij]
             #self.outputScreen()
+            
+
                 
-                
+    # procedure that prints L to screen
     def outputScreen(self):
         for i in xrange(- self.k, self.k +1):
             row = ''
@@ -196,6 +220,7 @@ class MinDifferenceEfficient():
                     row += "L(" + str(i) + ', '+ str(j) + ')=' + str(self.L[(i,j)]) + '\t'
             sys.stdout.write(row + '\n')
 
+    # procedure that prints L to file
     def outputFile(self, path):
         f = open(path,'w')
         for i in xrange(- self.k, self.k +1):
@@ -205,9 +230,13 @@ class MinDifferenceEfficient():
                     row += "L(" + str(i) + ', '+ str(j) + ')=' + str(self.L[(i,j)]) + '\t'
             f.write(row + '\n')
         f.close()
-
+        
+    # procedure that computes pattern analysis
     def computeMAXLENGTH(self):
-        self.MAXLENGTH = np.zeros((self.m,self.m),dtype = np.int)
+        self.MAXLENGTH = dict()
+        for i in xrange(self.m):
+            for j in xrange(self.m):
+                self.MAXLENGTH[(i,j)] = 0
         for d in xrange(self.m):
             if (self.R[self.m-d-1] == self.R[self.m-1]):
                 self.MAXLENGTH[(self.m-d-1,self.m-1)] = 1
@@ -222,7 +251,8 @@ class MinDifferenceEfficient():
                 #else:
                     #self.MAXLENGTH[(i,i+d)] = 0
                     #self.MAXLENGTH[(i+d,i)] = 0
-
+                    
+# procedure that prints usage statistics to file
 def printStatisticsToFile(path, stats):
     f = open(path,'a')
     line = ""
@@ -234,14 +264,13 @@ def printStatisticsToFile(path, stats):
     f.close()
 
 if __name__ == "__main__":
-
+    
+    #patternPath = sys.argv[1]
+    #textPath = sys.argv[2]
+    #k = int(sys.argv[3])
+    
     k = 4
-    #path = sys.argv[1]
     patternPath = "./Texts/pattern.fa"
-
-    """if (len(rows) != 2):
-        print "Use!"
-        sys.exit()"""
 
     R = parseFastaFile(patternPath)
     
@@ -261,12 +290,3 @@ if __name__ == "__main__":
         elapsed_time = str(round(time.time() - timeStart,3))
         #used_memory = memory_usage_psutil()
         printStatisticsToFile("output-python2.7.txt", [str(len(B)), str(len(R)), str(k), elapsed_time])#, used_memory])
-    
-    """mdi.computeMAXLENGTH()
-    x2 = mdi.MAXLENGTH
-    for i in xrange(mdi.m):
-        for j in xrange(mdi.m):
-            if x1[i,j] != x2[i,j]:
-                print (i,j)
-    print x1
-    print x2"""
